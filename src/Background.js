95d99e61bc8e12c8ae8b4c0088364300c0d1c5ea
Sion Kim <sk9uth@virginia.edu>
2021-03-02 20:54:26 -0500

    Added slideshow

--- src/Background.js
+++ src/Background.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './background.css';
+import Slideshow from './Slideshow'
 
 class Background extends Component {
     render() {
         return (
             <div className="Background">
                 <h1>Hello World</h1>
+                <Slideshow/>
             </div>
         );
     }
